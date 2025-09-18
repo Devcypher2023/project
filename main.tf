13098b83fd787b37efec4e8465b9fc32208decab
Devcypher2023 <charleston5000@yahoo.com>
2025-09-18 18:21:48 -0500

    up yo yo

--- main.tf
+++ main.tf
@@ -12,32 +12,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-# -----------------------------
-# Variables
-# -----------------------------
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "us-east-1"
-}
 
-variable "s3_bucket_name" {
-  description = "Base name for the S3 bucket"
-  type        = string
-  default     = "luffy-utrains-5000e"
-}
-
-variable "lambda_function_name" {
-  description = "Name of the Lambda function"
-  type        = string
-  default     = "userserverless-lambda"
-}
-
-variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB table"
-  type        = string
-  default     = "userserverless"
-}
 
 # -----------------------------
 # S3 Bucket for Static Website
